a0381453398617d8f540776c0dd9cefdf873d97b
SpiritCroc <dev@spiritcroc.de>
2019-10-13 15:13:07 +0200

    Update dependencies
    Change-Id: I68dba7ae15b243d32384d0863f1eacb5aed47073

--- app/src/main/java/de/spiritcroc/colorflash/SettingsActivity.java
+++ app/src/main/java/de/spiritcroc/colorflash/SettingsActivity.java
@@ -19,8 +19,8 @@
 package de.spiritcroc.colorflash;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
